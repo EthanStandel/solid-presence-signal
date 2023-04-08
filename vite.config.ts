44d132b123f519a6a68d1f6e01e62bb8e254a0e9
Ethan Standel <ethan@standel.io>
2023-04-08 12:24:25 -0400

    chore: build cleanup to make types match export

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), dts({ insertTypesEntry: true })],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
+    lib: {
+      entry: path.resolve(__dirname, "src/lib.ts"),
+      name: "solid-presence-signal",
+      fileName: (format) => `lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["solid-js"],
+    },
   },
 });
